2df46a757792088d5271b70be5d390c9f1f5f8f2
Christian Papathanasiou <chris@athcon.org>
2024-06-04 11:36:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ conn.commit()
 conn.close()
 
 
-@app.route('/executez',methods=['POST'])
+@app.route('/executxez',methods=['POST'])
 def haha():
    if request.method == 'POST':
      foo = request.form['foo']
