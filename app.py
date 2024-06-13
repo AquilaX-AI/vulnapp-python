621ab33df5a5e7e7a0ca9ff6737b70132044da52
Christian Papathanasiou <chris@athcon.org>
2024-06-13 17:55:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,42 +18,7 @@ c.execute('''CREATE TABLE IF NOT EXISTS posts
 conn.commit()
 conn.close()
 
-@app.route('/')
-def index():
-    conn = sqlite3.connect('blog.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM posts')
-    posts = c.fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
 
-@app.route('/add_post', methods=['GET', 'POST'])
-def add_post():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-
-        # Hardcoded credentials check (vulnerability)
-        if request.form['username'] == ADMIN_USERNAME and request.form['password'] == ADMIN_PASSWORD:
-            conn = sqlite3.connect('blog.db')
-            c = conn.cursor()
-            c.execute("INSERT INTO posts (title, content) VALUES ('" + title + "', '" + content + "')")
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-        else:
-            return "Unauthorized: Incorrect username or password."
-
-    return render_template('add_post.html')
-
-@app.route('/view_post/<int:id>')
-def view_post(id):
-    conn = sqlite3.connect('blog.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM posts WHERE id = ?', (id,))
-    post = c.fetchone()
-    conn.close()
-    return render_template('view_post.html', post=post)
 
 @app.route('/delete_post/<int:id>', methods=['POST'])
 def delete_post(id):
