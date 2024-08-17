fe8e300a846060c703552fe7fe618658f113d6f2
Christian Papathanasiou <cp@gcx.io>
2024-08-18 01:14:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key = os.urandom(24)
 
 # Hardcoded credentials (for demonstration purposes only)
 ADMIN_USERNAME = 'admin'
-ADMIN_PASSWORD = 'chris2f'
+ADMIN_PASSWORD = 'chris2ff'
 
 # Initialize the database
 conn = sqlite3.connect('blog.db')
