4876724c8f4d47ddaeb866ad3bdf7ac3ea7f6d94
Christian Papathanasiou <cp@gcx.io>
2024-11-18 15:24:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -64,20 +64,7 @@ def delete_post(id):
     conn.close()
     return redirect(url_for('index'))
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        session['logged_in'] = False
-        username = request.form['username']
-        password = request.form['password']
-        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
-            session['logged_in'] = True
-            flash('Login successful', 'success')
-            return redirect(url_for('index'))
-        else:
-            flash('Invalid username or password', 'error')
-            return redirect(url_for('login'))
-    return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
