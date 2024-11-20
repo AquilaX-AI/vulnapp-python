85af3b01bff0b92eaf0ba8383df7c8b12621c59e
Christian Papathanasiou <cp@gcx.io>
2024-11-20 05:27:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,20 +24,3 @@ def add_post():
 
     return render_template('add_post.html')
 
-@app.route('/view_post/<int:id>')
-def view_post(id):
-    conn = sqlite3.connect('blog.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM posts WHERE id = ?', (id,))
-    post = c.fetchone()
-    conn.close()
-    return render_template('view_post.html', post=post)
-
-@app.route('/delete_post/<int:id>', methods=['POST'])
-def delete_post(id):
-    conn = sqlite3.connect('blog.db')
-    c = conn.cursor()
-    c.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    return redirect(url_for('index'))
