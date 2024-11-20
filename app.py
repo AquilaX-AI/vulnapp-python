4d8bd6202690e20a0b7923a18dee3527bce9d7bd
Christian Papathanasiou <cp@gcx.io>
2024-11-20 12:29:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,27 @@ import subprocess
 
 
 
+@app.route('/')
+def index():
+    conn = sqlite3.connect('blog.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM posts')
+    posts = c.fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+
+
+    return render_template('add_post.html')
+
+@app.route('/view_post/<int:id>')
+def view_post(id):
+    conn = sqlite3.connect('blog.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM posts WHERE id = ?', (id,))
+    post = c.fetchone()
+    conn.close()
+    return render_template('view_post.html', post=post)
 
 @app.route('/delete_post/<int:id>', methods=['POST'])
 def delete_post(id):
